46f302b761baaaf6a3679a4e4129eaaab40bb4f2
Brett <bwkaiser@gmail.com>
2018-03-05 18:25:40 -0600

    Fixed associations on join tables, reseeding database next

--- app/models/type.rb
+++ app/models/type.rb
@@ -2,6 +2,6 @@ class Type < ApplicationRecord
   validates :name, :generation, presence: true
   validates :type_id, presence: true, uniqueness: true
 
-  has_many :pokemons
+  has_many :pokemon_types
   has_many :pokemons, through: :pokemon_types
 end
