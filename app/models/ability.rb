f3ed1e23392197b7fc9f95198541d75e79c438ce
Brett <bwkaiser@gmail.com>
2018-03-05 18:22:48 -0600

    Fixed associations from Pokemon to Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,6 +1,6 @@
 class Ability < ApplicationRecord
   validates :ability_id, presence: true, uniqueness: true
 
-  has_many :pokemon_abilities
-  has_many :pokemon, through: :pokemon_abilities
+  has_many :pokemons
+  has_many :pokemons, through: :pokemon_abilities
 end
